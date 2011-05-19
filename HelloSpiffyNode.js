a4fb067a24636f440453a3cb8e2c4e2b9cbfa680
zgrossbart@gmail.com <zgrossbart@gmail.com@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-05-19 13:02:10 +0000

    Did a little code clean up and changed the server to support index.html as the default welcome file.
    git-svn-id: https://spiffyui.googlecode.com/svn/samples/HelloSpiffyNode@941 39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c

--- HelloSpiffyNode.js
+++ HelloSpiffyNode.js
@@ -1,48 +1,51 @@
-    var sys = require("sys"),  
-        http = require("http"),  
-        url = require("url"),  
-        path = require("path"),  
-        fs = require("fs");  
+    var sys = require('sys'),  
+        http = require('http'),  
+        url = require('url'),  
+        path = require('path'),  
+        fs = require('fs');  
       
     http.createServer(function(request, response) {  
         var uri = url.parse(request.url).pathname;
 //        sys.puts("uri = " + uri);
         
         //process.cwd() gets the current working directory
-        var filename = path.join(process.cwd() + "/target/www", uri);  
+        var filename = path.join(process.cwd() + '/target/www', uri);  
         path.exists(filename, function(exists) {  
-            if(!exists) {  
+            if (!exists) {  
                 //this might be the REST call!
-                if (uri.indexOf("/simple/") == 0) {            
+                if (uri.indexOf('/simple/') == 0) {            
                 	var user = uri.substring(8);
                     var userAgent = request.headers['user-agent'];
-                    var payload = {user: user, userAgent: userAgent, serverInfo: "Node.js"};                                        
-                    response.writeHeader(200, {"Content-Type": "application/json"});  
+                    var payload = {user: user, userAgent: userAgent, serverInfo: 'Node.js'};                                        
+                    response.writeHeader(200, {'Content-Type': 'application/json'});  
                     response.end(JSON.stringify(payload));  
                     return;
                 } else {            
-                    response.writeHeader(404, {"Content-Type": "text/plain"});  
-                    response.end("404 Not Found\n"); 
+                    response.writeHeader(404, {'Content-Type': 'text/plain'});  
+                    response.end('404 Not Found\n'); 
                     return;
                 }
             }  
             //serve up the static file that is in /target/www
-            fs.readFile(filename, "binary", function(err, file) {  
+
+            if (('' + filename.match('/www/$')) === '/www/') {
+                // Then this is a request for the root and we'll return the index.html file
+                filename += 'index.html';
+            }
+            fs.readFile(filename, 'binary', function(err, file) {  
                 if(err) {  
-                    response.writeHeader(500, {"Content-Type": "text/plain"});  
+                    response.writeHeader(500, {'Content-Type': 'text/plain'});  
                     response.end(err + "\n");  
                     return;  
                 }  
       
                 response.writeHeader(200);  
-                response.write(file, "binary");  
+                response.write(file, 'binary');  
                 response.end();  
             });  
         });  
     }).listen(8181);  
     
-    sys.puts("========================================================================");
-    sys.puts("");
-    sys.puts("Access Hello Spiffy Node at http://localhost:8181/index.html");
-    sys.puts("");
-    sys.puts("========================================================================");
+    sys.puts('========================================================================\n');
+    sys.puts('Access Hello Spiffy Node at http://localhost:8181\n');
+    sys.puts('========================================================================\n');
