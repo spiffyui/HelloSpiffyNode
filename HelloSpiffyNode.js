e146e9657d4011b3d0b592f1c716fff4a5398c8b
zgrossbart@gmail.com <zgrossbart@gmail.com@39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c>
2011-05-20 14:22:48 +0000

    This change refactors the HelloSpiffyNode example into a single object with local variables.  It also fixed all of the JSLint errors for this example.
    git-svn-id: https://spiffyui.googlecode.com/svn/samples/HelloSpiffyNode@962 39d08bbd-2a53-3dc1-4a5e-8fe2b6c2d34c

--- HelloSpiffyNode.js
+++ HelloSpiffyNode.js
@@ -15,192 +15,210 @@ var targetDir = '/target/www';
 var i18nDir = '/js/lib/i18n/';
 
 /*
- * Global variables
+ * Our HelloSpiffyNode object defines our server and routines it needs
+ * to deliver our client and respond to REST requests.
  */
-var resources;
+var HelloSpiffyNode = {
+    resources: null,
 
-/*
- * Now we do a little sanity check to make sure the client is built before
- * we try to run the server.
- */
-var targetDirFull = path.join(process.cwd(), targetDir);
-path.exists(targetDirFull, function(exists) {
-    if (exists) {
-        runServer();
-    } else {
-        sys.puts('========================================================================\n');
-    	sys.puts('You have to build the Spiffy UI client before running the server.\n');
-        sys.puts('Run the ant in the current directory and then run the server again.\n');
-    	sys.puts('========================================================================\n');
-    }
-});
+    init: function() {
+        /*
+         * Now we do a little sanity check to make sure the client is built before
+         * we try to run the server.
+         */
+        var targetDirFull = path.join(process.cwd(), targetDir);
+        path.exists(targetDirFull, function(exists) {
+            if (exists) {
+                HelloSpiffyNode.runServer();
+            } else {
+                sys.puts('========================================================================\n');
+                sys.puts('You have to build the Spiffy UI client before running the server.\n');
+                sys.puts('Run the ant in the current directory and then run the server again.\n');
+                sys.puts('========================================================================\n');
+            }
+        });
+    },
 
-/**
- * Start the server
- */
-function runServer() {
-    /*
-     * Create the HTTP server
+    /**
+     * Start the server
      */
-    var server = http.createServer(function(request, response) {  
+    runServer: function() {
         /*
-         * Handle based on the URI
+         * Create the HTTP server
          */
-    	var uri = url.parse(request.url).pathname;
-        
-        /*
-         * Try to find a static file that matches the 
-         * current working directory/target/www/file name
-         * 
-         * (process.cwd() gets the current working directory)
-         */
-        var filename = path.join(process.cwd(), targetDir, uri);  
-        path.exists(filename, function(exists) {  
-        	/*
-        	 * If there's no file, it could be the REST call or
-        	 * an internationalized date JavaScript file or
-        	 * 404
-        	 */
-            if (!exists) {  
-                if (uri.indexOf('/simple/') === 0) {            
-                    /*
-                     * This is the REST call!
-                     */
-                	var user = uri.substring(8);
-                    var userAgent = request.headers['user-agent'];
-                    var payload = {user: user, userAgent: userAgent, serverInfo: 'Node.js'};                                        
-                    response.writeHeader(200, {'Content-Type': 'application/json'});  
-                    response.end(JSON.stringify(payload));  
-                    return;
-                    
-                } else if (uri === i18nDir + 'date' || uri === i18nDir + 'jquery.ui.datepicker.js') {
-                	/* 
-                	 * This is a internationalized file request!
-                	 */
-                	var resource  = getI18nDateResource(request, uri);
-                	filename = path.join(process.cwd(), targetDir, i18nDir, resource);
-                	/*
-                	 * Do not return -- let it get the correct i18n date file below
-                	 */
-                	
-                } else {
-                	/*
-                	 * 404!
-                	 */
-                    response.writeHeader(404, {'Content-Type': 'text/plain'});  
-                    response.end('404 Not Found\n'); 
-                    return;
-                }
-            }  
+        var server = http.createServer(function(request, response) {  
+            /*
+             * Handle based on the URI
+             */
+            var uri = url.parse(request.url).pathname;
             
             /*
-             * Serve up the static file that is in /target/www
+             * Try to find a static file that matches the 
+             * current working directory/target/www/file name
+             * 
+             * (process.cwd() gets the current working directory)
              */
-            
-            if (('' + filename.match('/www/$')) === '/www/') {
-                // Then this is a request for the root and we'll return the index.html file
-                filename += 'index.html';
-            }
-                    
-            fs.readFile(filename, 'binary', function(err, file) {  
-                if(err) {  
-                    response.writeHeader(500, {'Content-Type': 'text/plain'});  
-                    response.end(err + '\n');  
-                    return;  
+            var filename = path.join(process.cwd(), targetDir, uri);  
+            path.exists(filename, function(exists) {  
+                /*
+                 * If there's no file, it could be the REST call or
+                 * an internationalized date JavaScript file or
+                 * 404
+                 */
+                if (!exists) {  
+                    if (uri.indexOf('/simple/') === 0) {            
+                        /*
+                         * This is the REST call!
+                         */
+                        var user = uri.substring(8);
+                        var userAgent = request.headers['user-agent'];
+                        var payload = {user: user, userAgent: userAgent, serverInfo: 'Node.js'};                                        
+                        response.writeHeader(200, {'Content-Type': 'application/json'});  
+                        response.end(JSON.stringify(payload));  
+                        return;
+                        
+                    } else if (uri === i18nDir + 'date' || uri === i18nDir + 'jquery.ui.datepicker.js') {
+                        /* 
+                         * This is a internationalized file request!
+                         */
+                        var resource  = HelloSpiffyNode.getI18nDateResource(request, uri);
+                        filename = path.join(process.cwd(), targetDir, i18nDir, resource);
+                        /*
+                         * Do not return -- let it get the correct i18n date file below
+                         */
+                        
+                    } else {
+                        /*
+                         * 404!
+                         */
+                        response.writeHeader(404, {'Content-Type': 'text/plain'});  
+                        response.end('404 Not Found\n'); 
+                        return;
+                    }
                 }  
                 
-                response.writeHeader(200);  
-                response.write(file, 'binary');  
-                response.end();  
-            }); //end fs.readFile callback
-        }); //end path.exists callback
-    }); 
-
-    /*
-     * Load all the internationalized resources by reading the i18n directory
-     */
-    fs.readdir(path.join(process.cwd(), targetDir,  i18nDir), function(err, files) {
-    	resources = files;
-    	
-    	/*
-    	 * After resources are retrieved then the server can listen
-    	 */
-    	server.listen(port);
-    	
-    	sys.puts('========================================================================\n');
-    	sys.puts('Access Hello Spiffy Node at http://localhost:' + port + '\n');
-    	sys.puts('========================================================================\n');
+                /*
+                 * Serve up the static file that is in /target/www
+                 */
+                
+                if (filename.match('/www/$') !== null) {
+                    // Then this is a request for the root and we'll return the index.html file
+                    filename += 'index.html';
+                }
+                        
+                fs.readFile(filename, 'binary', function(err, file) {  
+                    if(err) {  
+                        response.writeHeader(500, {'Content-Type': 'text/plain'});  
+                        response.end(err + '\n');  
+                        return;  
+                    }  
+                    
+                    response.writeHeader(200);  
+                    response.write(file, 'binary');  
+                    response.end();  
+                }); //end fs.readFile callback
+            }); //end path.exists callback
+        }); 
     
-    });
-}
+        /*
+         * Load all the internationalized resources by reading the i18n directory
+         */
+        fs.readdir(path.join(process.cwd(), targetDir,  i18nDir), function(err, files) {
+            HelloSpiffyNode.resources = files;
+            
+            /*
+             * After resources are retrieved then the server can listen
+             */
+            server.listen(port);
+            
+            sys.puts('========================================================================\n');
+            sys.puts('Access Hello Spiffy Node at http://localhost:' + port + '\n');
+            sys.puts('========================================================================\n');
+        
+        });
+    },
 
-/**
- * Return the internationalized date file name
- * @param request - the http request
- * @param uri - the uri of the request
- * @returns {String} - the file name
+    /**
+     * Return the internationalized date file name
+     * @param request - the http request
+     * @param uri - the uri of the request
+     * @returns {String} - the file name
+     */
+    getI18nDateResource: function(request, uri) {
+        var resourceName = uri.substring(uri.lastIndexOf('/') + 1);
+        
+        if (resourceName.match('.js$') !== null) {
+            /*
+             * We start by taking the .js file extension off of the
+             * filename
+             */
+            resourceName = resourceName.substring(0, resourceName.length - 3);
+        }
+
+        /*
+         * Check for the file with the matching locale
+         */
+        var langs = request.headers['accept-language'];
+        var locales = langs.split(',');
+        var i = 0;
+
+        for (i = 0; i < locales.length; i++) {
+            var locale = locales[i];
+            /*
+             * Trim off ";q=0.3", if exists
+             */
+            var loc = locale.indexOf(';') > 0 ? locale.substring(0, locale.indexOf(';')) : locale;
+            
+            /*
+             * Capitalize the country to match file name, if exists
+             */
+            var langCountry = loc.split('-');
+            if (langCountry.length === 2) {
+                langCountry[1] = langCountry[1].toUpperCase();          
+                //rejoin loc
+                loc = langCountry.join('-');
+            }
+            
+            var resource = resourceName + '-' + loc + '.js';
+            if (HelloSpiffyNode.resourceExists(resource)) {
+                return resource;
+            } else {
+                /*
+                 * Try just on country
+                 */
+                resource = resourceName + '-' + langCountry[0] + '.js';
+                if (HelloSpiffyNode.resourceExists(resource)) {
+                    return resource;
+                }
+            }
+            /*
+             * Hasn't found a match, so try next locale
+             */
+        }
+        /*
+         * No matches, return default
+         */
+        return resourceName + '-en.js';
+    },
+
+    /**
+     * Check if the resource is in the resources global variable
+     * @param resource to test
+     * @returns {Boolean} true if found
+     */
+    resourceExists: function(resource) {
+        var i = 0;
+        for (i = 0; i < HelloSpiffyNode.resources.length; i++) {
+            if (HelloSpiffyNode.resources[i] === resource) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+/*
+ * Now that we've defined everything we need the last step is to 
+ * start our server.
  */
-function getI18nDateResource(request, uri) {
-	var resourceName = uri.substring(uri.lastIndexOf('/') + 1);
-	if (resourceName === 'jquery.ui.datepicker.js') {
-		resourceName = 'jquery.ui.datepicker';
-	}
-
-	/*
-	 * Check for the file with the matching locale
-	 */
-	var langs = request.headers['accept-language'];
-	var locales = langs.split(',');
-	
-	for (i in locales) {
-		var locale = locales[i];
-		/*
-		 * Trim off ";q=0.3", if exists
-		 */
-		var loc = locale.indexOf(';') > 0 ? locale.substring(0, locale.indexOf(';')) : locale;
-		
-		/*
-		 * Capitalize the country to match file name, if exists
-		 */
-		var langCountry = loc.split('-');
-		if (langCountry.length === 2) {
-			langCountry[1] = langCountry[1].toUpperCase();			
-			//rejoin loc
-			loc = langCountry.join('-');
-		}
-		
-		var resource = resourceName + '-' + loc + '.js';
-		if (resourceExists(resource)) {
-			return resource;
-		} else {
-			/*
-			 * Try just on country
-			 */
-			resource = resourceName + '-' + langCountry[0] + '.js';
-			if (resourceExists(resource)) {
-				return resource;
-			}
-		}
-		/*
-		 * Hasn't found a match, so try next locale
-		 */
-	}
-	/*
-	 * No matches, return default
-	 */
-	return resourceName + '-en.js';
-}
-
-/**
- * Check if the resource is in the resources global variable
- * @param resource to test
- * @returns {Boolean} true if found
- */
-function resourceExists(resource) {
-	for (i in resources) {
-		if (resources[i] === resource) {
-			return true;
-		}
-	}
-	return false;
-}
+HelloSpiffyNode.init();
